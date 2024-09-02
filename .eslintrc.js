9c50e07e5b3a3b355bf9a4c21e5e262c5be36f8a
AtakanG7 <pwxcv7352@gmail.com>
2024-09-02 16:53:16 +0300

    adding azure pipeline file

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: 'airbnb-base',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    rules: {
+      // Add any ESLint rules you want to apply
+    },
+  };
+  
